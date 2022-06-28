9d5af5295c4354b987a055d5f374cd956f835035
Víctor Olvera <vhocar@gmail.com>
2022-06-28 08:31:57 -0700

    Testing

--- staging.js
+++ staging.js
@@ -1,14 +1,22 @@
 import chalk from "chalk"
+const readline = require( 'readline' )
+const confirm = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout,
+})
 
 const args = process.argv.slice(2)
 
 if ( args[0] === 'pull' ) {
     console.log( chalk.yellow( 'WARNING: This will overwrite the staging website with the production site files and database.'  ) )
-    if ( confirm( chalk.yellow( 'Do you want to proceed? y/n'  ) ) ) {
-        console.log( 'Pulling files and database from the production site...' )
-    } else {
-        console.log( 'Production to Staging pull cancelled.' )
-    }
+    
+    confirm.question( chalk.yellow('Do you weant to proceed?'), answer => {
+        if ( answer == 'y' || answer == 'yes' ) {
+            console.log( 'Pulling files and database from production to staging...' )
+        } else {
+            console.log( 'Production to Staging pull cancelled.' )
+        }
+    } )
 
 }
 
