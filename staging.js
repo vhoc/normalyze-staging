4030c9dd8d1e9c6fe8305a1b0300f2298199f9fc
Víctor Olvera <vhocar@gmail.com>
2022-06-28 08:13:35 -0700

    new file

---
+++ staging.js
@@ -0,0 +1,9 @@
+const arguments = argv
+
+if ( arguments[0] === 'pull' ) {
+    console.log( 'pulling files from production' )
+}
+
+if ( arguments[0] === 'push' ) {
+    console.log( 'pulling files to production' )
+}
