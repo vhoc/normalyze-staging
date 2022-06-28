21f03b3197c9b4a38ada355fcfdf38f89dcee991
Víctor Olvera <vhocar@gmail.com>
2022-06-28 14:48:39 -0700

    SQl url replacements

--- pull.js
+++ pull.js
@@ -14,22 +14,28 @@ const pull = async ( files, databases ) => {
     
     confirm.question( chalk.yellow('Do you weant to proceed? y/n: '), answer => {
         if ( answer == 'y' || answer == 'yes' ) {
-            console.log( 'Pulling files from production to staging...' )
-            fs.copySync( files.production, files.staging, {
-                overwrite: true
-            }, error => {
-                if ( error ) {
-                    console.error( error )
-                    process.exit()
-                } else {
-                    const timestamp = timeStamp()
-                    console.log( `Exported the database into a temporary file...` )
-                    exec( `sudo mysqldump -u root ${ databases.production } > ${ databases.backupPath }_${ timestamp }` )
-                    console.log( `Database exported.` )
-                    console.log( chalk.greenBright( `All files have been copied from Production to Staging!` ) )
-                    process.exit()
-                }
-            } )
+            const timestamp = timeStamp()
+
+            console.log( 'Copying files from Production to Staging...' )
+            exec( `cp -R ${ files.production }/* ${ files.staging }` )
+
+            console.log( `Copy done. Cloning Production's permissions into Staging...` )
+            exec( `rsync -ar ${ files.production } ${ files.staging }` )
+
+            console.log( `File permissions applied. Backing up Production's database...` )
+            exec( `mysqldump -u root ${ databases.production } > ${ databases.backupPath }_${ timestamp }.sql` )
+
+            console.log( `Database backed up. Importing Production database into the Staging database...` )
+            exec( `mysql ${ databases.staging } < ${ databases.backupPath }_${ timestamp }.sql` )
+
+            console.log( `Production database imported into the Staging database. Updating the URL in the whole database...` )
+            exec( `mysql -e "UPDATE wp_options SET option_value = replace( option_value, 'https://normalyze.ai', 'https://staging.normalyze.ai' );"` )
+            exec( `mysql -e "UPDATE wp_posts SET guid = replace(guid, 'https://normalyze.ai','https://staging.normalyze.ai');"` )
+            exec( `mysql -e "UPDATE wp_posts SET post_content = replace(post_content, 'https://normalyze.ai', 'https://staging.normalyze.ai');"` )
+            exec( `mysql -e "UPDATE wp_postmeta SET meta_value = replace(meta_value,'https://normalyze.ai','https://staging.normalyze.ai');"` )
+
+            console.log( chalk.bgGreenBright( `Staging environment has been updated to the latest Production state!` ) )
+            process.exit()
     
         } else {
             console.log( 'Production to Staging pull cancelled.' )
