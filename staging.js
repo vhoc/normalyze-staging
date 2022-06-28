0a1eae0ee35afc6fe8568cf8219ed98118d15034
Víctor Olvera <vhocar@gmail.com>
2022-06-28 08:23:46 -0700

    process.argv

--- staging.js
+++ staging.js
@@ -1,9 +1,17 @@
-const arguments = process.argv.slice(2)
+import chalk from "chalk"
 
-if ( arguments[0] === 'pull' ) {
-    console.log( 'pulling files from production' )
+const args = process.argv.slice(2)
+
+if ( args[0] === 'pull' ) {
+    console.log( chalk.yellow( 'WARNING: This will overwrite the staging website with the production site files and database.'  ) )
+    if ( confirm( chalk.yellow( 'Do you want to proceed? y/n'  ) ) ) {
+        console.log( 'Pulling files and database from the production site...' )
+    } else {
+        console.log( 'Production to Staging pull cancelled.' )
+    }
+
 }
 
-if ( arguments[0] === 'push' ) {
-    console.log( 'pulling files to production' )
+if ( args[0] === 'push' ) {
+    console.log( 'pushing files to production' )
 }
